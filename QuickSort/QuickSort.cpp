0dfce2ebc988a52fd61de9c8563f1a8cfd06f892
Alan21103 <syahlansaputra03@gmail.com>
2023-03-30 16:32:20 +0700

    Prosedur Input

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -5,3 +5,27 @@ using namespace std;
 int arr[20];							// Array of integers to hold values
 int cmp_count = 0;						// Number of comparasion
 int mov_count = 0;						// Number of data movement
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; 1 < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[1];
+	}
+}
